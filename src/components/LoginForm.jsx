227481144dbed586c4670ad66cdd60dc45d16f2f
R <ramz9_97@hotmail.com>
2023-04-25 23:15:23 +0300

    auth page ui

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,12 +1,18 @@
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, Box, Stack } from '@mui/material';
 import React from 'react';
 
 export const LoginForm = ({ values, handleChange, handleSubmit }) => {
   return (
     <form onSubmit={handleSubmit}>
-      <TextField name="username" label="Username" value={values.username} onChange={handleChange} />
-      <TextField name="password" label="Password" type="password" value={values.password} onChange={handleChange} />
-      <Button type="submit">Login</Button>
+      <Box>
+        <Stack spacing={2}>
+          <TextField name="username" label="Username" value={values.username} onChange={handleChange} />
+          <TextField name="password" label="Password" type="password" value={values.password} onChange={handleChange} />
+          <Button variant="contained" type="submit">
+            Login
+          </Button>
+        </Stack>
+      </Box>
     </form>
   );
 };
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, Box, Stack } from '@mui/material';
 import React from 'react';
 
 export const RegisterForm = ({ values, handleChange, handleSubmit }) => {
@@ -6,11 +6,16 @@ export const RegisterForm = ({ values, handleChange, handleSubmit }) => {
     <form onSubmit={handleSubmit}>
       <p>usernames must be min 3 char and max 20 and is numeric</p>
       <p>password must be min 6 char</p>
-
-      <TextField name="email" label="Email" type="email" value={values.email} onChange={handleChange} />
-      <TextField name="username" label="Username" value={values.username} onChange={handleChange} />
-      <TextField name="password" label="Password" type="password" value={values.password} onChange={handleChange} />
-      <Button type="submit">Register</Button>
+      <Box>
+        <Stack spacing={2}>
+          <TextField name="email" label="Email" type="email" value={values.email} onChange={handleChange} />
+          <TextField name="username" label="Username" value={values.username} onChange={handleChange} />
+          <TextField name="password" label="Password" type="password" value={values.password} onChange={handleChange} />
+          <Button variant="contained" type="submit">
+            Register
+          </Button>
+        </Stack>
+      </Box>
     </form>
   );
 };
