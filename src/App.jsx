30b3d533d19bdc8ca132bba161403d6162e7431b
R <63073421+ramizwd@users.noreply.github.com>
2023-04-26 00:39:44 +0300

    basic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { AuthPage } from './pages/Auth';
+import { HomePage } from './pages/Home';
+import MainNavigation from './components/MainNavigation';
 
 function App() {
   return (
     <div className="App">
-      <AuthPage />
+      <MainNavigation />
+      <Routes>
+        <Route path="/auth" element={<AuthPage />}></Route>
+        <Route path="/home" element={<HomePage />}></Route>
+      </Routes>
     </div>
   );
 }
